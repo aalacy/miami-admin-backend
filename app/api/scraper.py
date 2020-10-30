f163983ff4832b419f49a96bd096d59cd5d996ca
aalacy <aaronlacywhite387@gmail.com>
2020-10-30 16:24:20 -0700

    update api

--- app/api/scraper.py
+++ app/api/scraper.py
@@ -74,7 +74,7 @@ class Driver():
 		chrome_prefs["profile.managed_default_content_settings"] = {"images": 2}
 		options.add_argument('headless')
 		path = f"{self.basedir}/data/chromedriver"
-		chrome = Chrome(executable_path=path, options=options)
+		chrome = Chrome(options=options)
 
 		chrome.implicitly_wait(20)
 		self.driver = chrome
